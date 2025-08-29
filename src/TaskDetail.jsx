21a1152c0225899ae7e6ed1055f41d56b11960c2
gabrielgoncalvesvitoretti <gabrielvitoretti@icloud.com>
2025-08-29 07:47:04 -0300

    commit teste

--- src/TaskDetail.jsx
+++ src/TaskDetail.jsx
@@ -1,12 +1,12 @@
 import { ChevronLeftIcon } from "lucide-react";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import Title from "../componets/Title";
+import Title from "./componets/Title";
 
 function TaskDetail() {
   const [searchParams] = useSearchParams();
-  
+
   const title = searchParams.get("title");
-  
+
   const description = searchParams.get("description");
 
   const navigate = useNavigate();
@@ -15,12 +15,13 @@ function TaskDetail() {
     <div className="w-screen h-screen bg-slate-500 p-6 justify-center flex">
       <div className="w-[500px] space-y-4">
         <div className="flex justify-center relative mb-6 text-slate-100">
-            <button onClick={() => navigate(-1)} className="absolute left-0 bottom-0">
-                <ChevronLeftIcon/>
-            </button>
-          <Title>
-            Task Detail
-          </Title>
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute left-0 bottom-0"
+          >
+            <ChevronLeftIcon />
+          </button>
+          <Title>Task Detail</Title>
         </div>
         <div className="bg-slate-200 p-4 rounded-md">
           <h1 className="text-xl font-bold text-slate-600">{title}</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import TaskDetail from "./pages/TaskDetail.jsx";
+import TaskDetail from "./TaskDetail.jsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
